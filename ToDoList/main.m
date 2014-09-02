02092ac7b8a36146809369bf2d08c0757923a6d8
yxtonic <yxtonic@gmail.com>
2014-09-02 15:01:26 -0700

    scene 1 added

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Toni Chen on 9/2/14.
+//  Copyright (c) 2014 ToniChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
